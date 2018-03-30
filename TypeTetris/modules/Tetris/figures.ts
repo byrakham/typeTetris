6a073539921b4285828f7ba93cad13090452daaf
byrakham <byrakham@gmail.com>
2018-03-30 20:21:22 +0300

    added solution

---
+++ TypeTetris/modules/Tetris/figures.ts
@@ -0,0 +1,28 @@
+﻿import * as geometry from "./geometry";
+
+export interface IFigurePoint {
+    x: number;
+    y: number;
+}
+
+export interface IFigure {
+    Points: Array<IFigurePoint>;
+}
+
+class RGBAColor {
+    constructor(public r: number, public g: number, public b: number, public a: number = 255) { }
+}
+
+abstract class Geometry {
+    Position: geometry.Point3D;
+    Vertices: Array<geometry.Point3D>;
+    Faces: Array<number>;
+
+}
+
+class Cube extends Geometry {
+    
+    Stroke: RGBAColor;
+    Fill: RGBAColor = null;
+
+}
---
+++ TypeTetris/modules/Tetris/tetris.ts
@@ -0,0 +1,43 @@
+﻿
+
+
+
+
+
+
+export class Tetris {
+
+    Rows: number;
+    Columns: number;
+}
+
+export class TetrisLogic {
+
+
+}
+
+export interface IGame {
+
+    Start(): void;
+    Pause(): void;
+    Resume(): void;
+    Stop(): void;
+}
+
+export class TetrisGame implements IGame {
+    Start(): void {
+        var tetris: Tetris = new Tetris();
+
+    }
+    Pause(): void {
+        throw new Error("Method not implemented.");
+    }
+    Resume(): void {
+        throw new Error("Method not implemented.");
+    }
+    Stop(): void {
+        throw new Error("Method not implemented.");
+    }
+
+
+}
